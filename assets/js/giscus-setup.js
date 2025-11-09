5962851ee4c5dcc3cc56849b5be442a0c130a650
jordialt <94012776+jordialt@users.noreply.github.com>
2025-11-09 22:54:23 +0000

    Deploying to gh-pages from @ jordialt/jordialt.github.io@44ac4d45042d41d8bd138e5d29ca089685c4fe52 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "jordialt/jordialtCV.github.io",
+    "data-repo": "jordialt/jordialt.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
