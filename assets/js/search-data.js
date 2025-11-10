b304475d7b3c8e9bafde759195ab3fa17decab60
jordialt <94012776+jordialt@users.noreply.github.com>
2025-11-10 03:10:54 +0000

    Deploying to gh-pages from @ jordialt/jordialt.github.io@6430b6eec0ae9ef8fd9f74749b50c44be24b3482 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Here&#39;s an overview of my background and my relevant skills.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
