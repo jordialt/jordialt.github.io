837405e8916792809d0444bdd0799112a30a1beb
jordialt <94012776+jordialt@users.noreply.github.com>
2025-11-10 14:41:11 +0000

    Deploying to gh-pages from @ jordialt/jordialt.github.io@1327bee0e415663b619705b88e166f1d63186e18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,7 +10,7 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of my cool projects.",
           section: "Navigation",
           handler: () => {
